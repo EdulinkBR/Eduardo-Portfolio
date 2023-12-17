387cc5475a7e4bd16d24a94873dd8516b4cd17e0
EdulinkBR <edulink@hotmail.com.br>
2023-12-17 12:54:39 -0300

    Major Update: Contatos

--- contato.js
+++ contato.js
@@ -1,37 +1,49 @@
-function validarCampos() {
-    // Resetar mensagens de erro
-    resetarMensagensErro();
-
-    // Verificar se os campos obrigatórios estão preenchidos
-    let camposValidos = true;
-
-    const camposObrigatorios = document.querySelectorAll('.campo-obrigatorio');
-    camposObrigatorios.forEach(campo => {
-        const idCampo = campo.id;
-        const mensagemErroId = `erro${idCampo.charAt(0).toUpperCase()}${idCampo.slice(1)}`;
-        const mensagemErro = document.getElementById(mensagemErroId);
-
-        if (!campo.value.trim()) {
-            mensagemErro.textContent = 'Campo obrigatório';
-            mensagemErro.style.color = 'red';
-            camposValidos = false;
-        }
-    });
-
-    if (camposValidos) {
-        // Se os campos são válidos, você pode adicionar aqui o código para enviar o formulário (se necessário)
-        // Exemplo: document.getElementById('meuFormulario').submit();
-
-        // Impedir o comportamento padrão do formulário (redirecionamento)
-        return false;
+function validarCampos(formularioId) {
+        // Resetar mensagens de erro
+        resetarMensagensErro(formularioId);
+    
+        // Verificar se os campos obrigatórios estão preenchidos
+        let camposValidos = true;
+    
+        const camposObrigatorios = document.querySelectorAll(`#${formularioId} .campo-obrigatorio`);
+        camposObrigatorios.forEach(campo => {
+            const idCampo = campo.id;
+            const mensagemErroId = `erro${idCampo.charAt(0).toUpperCase()}${idCampo.slice(1)}`;
+            const mensagemErro = document.getElementById(mensagemErroId);
+    
+            if (!campo.value.trim()) {
+                mensagemErro.textContent = 'Campo obrigatório';
+                mensagemErro.style.color = 'red';
+                camposValidos = false;
+            }
+        });
+    
+        if (camposValidos) {
+            // Se os campos são válidos, você pode adicionar aqui o código para enviar o formulário (se necessário)
+            // Exemplo: document.getElementById(formularioId).submit();
+    
+            // Enviar o e-mail
+            // ...
+    
+            // Exibir uma mensagem de sucesso
+            const mensagemSucesso = document.getElementById("mensagemSucesso");
+            mensagemSucesso.style.display = "block";
+    
+            // Remover o botão
+            const botaoFechar = mensagemSucesso.querySelector("button");
+            botaoFechar.remove();
+    
+            // Permitir o comportamento padrão do formulário (redirecionamento)
+            return true;
+        }
+    
+        return camposValidos;
     }
-
-    return camposValidos;
-}
-
-function resetarMensagensErro() {
-    const mensagensErro = document.querySelectorAll('.mensagem-erro');
-    mensagensErro.forEach(mensagem => {
-        mensagem.textContent = '';
-    });
-}
+    
+    function resetarMensagensErro(formularioId) {
+        const mensagensErro = document.querySelectorAll(`#${formularioId} .mensagem-erro`);
+        mensagensErro.forEach(mensagem => {
+            mensagem.textContent = '';
+        });
+    }
+    
